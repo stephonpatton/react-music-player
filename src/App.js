f90cfeff5ba8f13ae2a7ba5e0bb05d8c3113b43e
Stephon Patton <spatton6@asu.edu>
2021-04-05 16:55:45 -0400

    - Inital component creation done - Styling with sass done for player and song (for now)

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import Player from './components/Player';
 import Song from './components/Song';
 
+import "./styles/app.scss"
+
 function App() {
   return (
     <div className="App">
-        <Player/>
         <Song/>
+        <Player/>
+ 
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlay, faAngleLeft, faAngleRight} from "@fortawesome/free-solid-svg-icons";
+
 
 const Player = () => {
     return (
-        <div>
+        <div className="player">
+
             <div className="time-control">
                 <p>Start Time</p>
                 <input type="range"/>
@@ -11,9 +14,10 @@ const Player = () => {
             </div>
 
             <div className="play-control">
-
+                <FontAwesomeIcon className="skip-back" size = "2x" icon={faAngleLeft} onClick={() => alert("HELLO")}/>
+                <FontAwesomeIcon className="play" size="2x" icon={faPlay} onClick={() => alert("HELLO")}/>
+                <FontAwesomeIcon className="skip-forward" size = "2x" icon={faAngleRight} onClick={() => alert("HELLO")}/>
             </div>
-
 
         </div>
     );
